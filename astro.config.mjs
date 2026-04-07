2a0f89d2092fa86109e830e3d8bbcf50f0d7f968
Rishav Dhar <19497993+rdhar@users.noreply.github.com>
2026-04-07 17:23:29 +0100

    Add default content security policy (#8)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,27 +5,32 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.hotfix.day',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-					},
-				],
-			},
-		},
-	],
+  site: 'https://www.hotfix.day',
+  integrations: [mdx(), sitemap()],
+  security: {
+    csp: {
+      directives: ["default-src 'self'"],
+    },
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Atkinson',
+      cssVariable: '--font-atkinson',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/atkinson-regular.woff'],
+            weight: 400,
+            style: 'normal',
+          },
+          {
+            src: ['./src/assets/fonts/atkinson-bold.woff'],
+            weight: 700,
+            style: 'normal',
+          },
+        ],
+      },
+    },
+  ],
 });
